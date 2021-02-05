14ec0e86891c7a1580bfe2b245eb790c07f15c18
Alamgir <ourandoc@gmail.com>
2021-02-05 14:06:52 +0500

    Add model and controller for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_075000) do
+ActiveRecord::Schema.define(version: 2021_02_05_085327) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
