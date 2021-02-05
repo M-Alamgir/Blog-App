5844d5227d5db5f7e1581544d3e4114b171c26ce
Alamgir <ourandoc@gmail.com>
2021-02-05 13:51:42 +0500

    Modified changes to styles and destroy, update method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,20 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
 
@@ -30,7 +25,6 @@ class PostsController < ApplicationController
       end
   end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
       if @post.update(post_params)
         redirect_to @post
@@ -39,22 +33,17 @@ class PostsController < ApplicationController
       end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to posts_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_post
       @post = Post.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def post_params
       params.require(:post).permit(:title, :body)
     end
