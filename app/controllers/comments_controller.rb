47720b912e507c03b79c89e68f008260d7406706
Alamgir <ourandoc@gmail.com>
2021-02-05 15:37:56 +0500

    Fix comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,11 @@ class CommentsController < ApplicationController
 
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(params[:comment]).permit(:name, :comment)
+        @comment = @post.comments.create(com_params)
         redirect_to post_path(@post)
+    end
+
+    def com_params
+        params.require(:comment).permit(:name, :comment)
     end
 end
